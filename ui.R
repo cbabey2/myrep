ea9de182bb1953da18e925142c3b04a0b6ae2851
Adam Berrones <51128421+berrones@users.noreply.github.com>
2026-01-13 09:30:41 -0500

    Add UI for Simple Shiny App with Data Frame

---
+++ ui.R
@@ -0,0 +1,20 @@
+# Define UI for application
+ui <- fluidPage(
+  titlePanel("Simple Shiny App with Hardcoded Data Frame"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      # Slider input to filter by minimum age
+      sliderInput("ageThreshold", 
+                  label = "Minimum Age:", 
+                  min = 20, 
+                  max = 50, 
+                  value = 25)
+    ),
+    
+    mainPanel(
+      # Output the table
+      tableOutput("table")
+    )
+  )
+)

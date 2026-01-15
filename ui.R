95a7a0843d4c03f12a71cdf49c11792761ad5665
Claire Babey <cbabey2@gmail.com>
2026-01-15 09:51:11 +0000

    Create seperate tabs

--- ui.R
+++ ui.R
@@ -1,40 +1,53 @@
 
 # Define UI for application
-ui <- fluidPage(
-  titlePanel("Name & Demographic Search by Age"),
+ui <- navbarPage(
+  title = "Age x City Search",
   
-  sidebarLayout(
-    sidebarPanel(
-      # Slider input to filter by minimum age
-      sliderInput("ageThreshold", 
-                  label = "Minimum Age:", 
-                  min = 20, 
-                  max = 50, 
-                  value = 25
-    ),
-
-      selectInput(
-      inputId = "city",
-      label = "City", 
-      choices = c("All", unique(df$City)),
-      selected = "All"
-    ),
-    
-      textInput(
-        inputId = "nameQuery",
-        label ="Name",
-        value = ""
+  tabPanel(
+    titlePanel("Name & Demographic Search by Age"),
+    sidebarLayout(
+      sidebarPanel(
+        # Slider input to filter by minimum age
+        sliderInput("ageThreshold", 
+                    label = "Minimum Age:", 
+                    min = 20, 
+                    max = 50, 
+                    value = 25
+        ),
+        
+        selectInput(
+          inputId = "city",
+          label = "City", 
+          choices = c("All", unique(df$City)),
+          selected = "All"
+        )
+      ),
+        mainPanel(
+          tableOutput("table")
+        )
+      )
+  ),
+  
+  
+    tabPanel(
+      titlePanel("Name Summary Statistics"),
+      sidebarLayout(
+        sidebarPanel(
+          textInput(
+           inputId = "nameQuery",
+           label ="Name",
+            value = ""
         )
       ),
 
   mainPanel(
-      # Output the table
-      tableOutput("table"),
       textOutput("name_message"),
       tableOutput("name_summary")
+      )
     )
   )
 )
+
 
 
 
